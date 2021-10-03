be4dede520209f6eea3273c6e20ba2d21676aae8
samconnects1 <samconnects@gmail.com>
2021-10-03 15:41:11 +1300

    Removed logging on js/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('Sample JS text.')
-
-
 const weatherForm = document.querySelector('form')
 const search=document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
